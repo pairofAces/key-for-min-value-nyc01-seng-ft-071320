41cb1db0ad273d6193f9cd068dbe6279de16d6da
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-29 18:53:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |name, num|
+  name_hash.select do |name, num|
     if num < 2
       puts "#{name}"
     end
