9e53a1dfb8ad59669b3456956ad9d038b2cc24e9
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-29 19:00:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,10 @@ def key_for_min_value(name_hash)
   lowest_k = nil
   lowest_v = nil
   name_hash.select do |name, num|
-    if lowest_v == nil || num < lowest_v
+    if lowest_v == nil 
       lowest_v = num
       lowest_k = name 
+    else 
     end
     lowest_k
   end
